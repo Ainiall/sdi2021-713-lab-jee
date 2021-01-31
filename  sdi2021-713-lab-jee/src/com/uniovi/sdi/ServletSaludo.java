a287e8b1c164a304000770ed6f7782fdef56e644
Ainiall <angela_l.lopez@hotmail.com>
2021-01-31 18:37:12 +0100

    SDI-2021-713-1.1-Los Servlets son multihilo

---
+++  sdi2021-713-lab-jee/src/com/uniovi/sdi/ServletSaludo.java
@@ -0,0 +1,71 @@
+package com.uniovi.sdi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletSaludo
+ */
+// declara  en  el  inicio  de  la  clase  nos  sirve  para  definir  la  URL  del servlet
+@WebServlet("/ServletSaludo")
+public class ServletSaludo extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    int contador = 0; // para verificar que se ejecutan en la misma instancia
+		      // del servlet
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletSaludo() {
+	super();
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+	// generado
+	response.getWriter().append("Served at: ")
+		.append(request.getContextPath());
+	// implementacion de respuesta doget
+	response.setContentType("text/html");
+	PrintWriter out = response.getWriter();
+	out.println("<HTML>");
+	out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+	out.println("<BODY>");
+	String nombre = (String) request.getParameter("nombre");
+	if (nombre != null) {
+	    out.println("Hola " + nombre + "<br>");
+	}
+	// para verificar que se ejecuta sobre la misma instancia del servlet
+	try{
+	    Thread.sleep(15000);
+	} catch (InterruptedException e) {}
+	out.println("ID del hilo:"+Thread.currentThread().getId()+"<br>");
+	contador++;
+	out.println("Visitas:"+contador+"<br>");
+	
+	out.println("</BODY></HTML>");
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+	// generado
+	// se encarga de que responda a peticiones POST
+	doGet(request, response);
+    }
+
+}
