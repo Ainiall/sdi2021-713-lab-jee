753f88bc9b8c57c891c7a7eb09dcd3506a22902d
Ainiall <angela_l.lopez@hotmail.com>
2021-03-01 23:44:26 +0100

    SDI-2021-713-4.2-Roles.

---
+++ sdi2021-713-spring/src/main/java/com/uniovi/services/RolesService.java
@@ -0,0 +1,14 @@
+package com.uniovi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RolesService {
+    // lo ideal sería incluir una entidad Role en bases de datos y relacionarla
+    // con la entidad user
+    String[] roles = { "ROLE_STUDENT", "ROLE_PROFESSOR", "ROLE_ADMIN" };
+
+    public String[] getRoles() {
+	return roles;
+    }
+}
